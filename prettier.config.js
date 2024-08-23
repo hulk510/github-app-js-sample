85c427391659ba94aafee7831c185c8b70772119
hulk510 <dorian.51069@gmail.com>
2024-08-23 10:37:54 +0900

    fix: lintに失敗するのでformat修正

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const Config = {
+  semi: false,
+  trailingComma: 'none',
+  singleQuote: true
+}
+
+export default Config
